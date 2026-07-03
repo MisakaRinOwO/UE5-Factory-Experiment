7ed6bd632dd950b6f076558e86c078cd79c471b4
Misaka Rin <misaka.rin.owo@gmail.com>
2026-07-03 19:16:31 +0800

    Added cell and chunk debug visual and controls

--- Source/FactoryExperiment/Private/Factory/FactoryManager.cpp
+++ Source/FactoryExperiment/Private/Factory/FactoryManager.cpp
@@ -1,17 +1,37 @@
 #include "Factory/FactoryManager.h"
 
+#include "Components/SceneComponent.h"
+#include "DrawDebugHelpers.h"
 #include "Engine/World.h"
 #include "Factory/Buildings/FactoryBuilding.h"
 #include "Factory/Data/FactoryBuildingDataAsset.h"
+#include "GameFramework/PlayerController.h"
+
+namespace
+{
+	constexpr float DebugGridZOffset = 5.0f;
+	constexpr float DebugCellLineThickness = 0.0f;
+	constexpr float DebugChunkLineThickness = 3.0f;
+	constexpr float DebugHoveredCellLineThickness = 5.0f;
+
+	const FColor DebugCellColor(192, 192, 192);
+	const FColor DebugChunkColor(160, 32, 240);
+	const FColor DebugHoveredCellColor(0, 255, 0);
+}
 
 AFactoryManager::AFactoryManager()
 {
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
+
+	TransformComponent = CreateDefaultSubobject<USceneComponent>(TEXT("TransformComponent"));
+	RootComponent = TransformComponent;
 }
 
 void AFactoryManager::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CreateInitialChunks();
 
 	if (SimulationStepInterval > 0.0f)
 	{
@@ -22,6 +42,21 @@ void AFactoryManager::BeginPlay()
 			SimulationStepInterval,
 			true
 		);
+	}
+}
+
+void AFactoryManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bUpdateHoveredCellFromMouseRaycast)
+	{
+		UpdateHoveredCellFromMouseRaycast();
+	}
+
+	if (bDrawDebugCells || bDrawDebugChunks || bDrawDebugHoveredCell)
+	{
+		DrawDebugGrid();
 	}
 }
 
@@ -50,11 +85,7 @@ bool AFactoryManager::TryPlaceBuilding(
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.Owner = this;
 
-	const FVector SpawnLocation = FVector(
-		OriginCoord.X * 100.0f,
-		OriginCoord.Y * 100.0f,
-		0.0f
-	);
+	const FVector SpawnLocation = GridCellCenterToWorld(OriginCoord);
 
 	AFactoryBuilding* BuildingActor = World->SpawnActor<AFactoryBuilding>(
 		BuildingData->BuildingActorClass,
@@ -196,6 +227,17 @@ TArray<FGridCoord> AFactoryManager::GetNeighbors(const FGridCoord& Coord) const
 	};
 }
 
+void AFactoryManager::SetDebugHoveredCell(const FGridCoord& Coord)
+{
+	DebugHoveredCellCoord = Coord;
+	bHasDebugHoveredCell = true;
+}
+
+void AFactoryManager::ClearDebugHoveredCell()
+{
+	bHasDebugHoveredCell = false;
+}
+
 void AFactoryManager::SimulationStep()
 {
 	UpdateConveyors(SimulationStepInterval);
@@ -208,6 +250,189 @@ void AFactoryManager::UpdateMachines(float /*DeltaTime*/)
 
 void AFactoryManager::UpdateConveyors(float /*DeltaTime*/)
 {
+}
+
+void AFactoryManager::CreateInitialChunks()
+{
+	Chunks.FindOrAdd(FGridCoord(0, 0));
+}
+
+void AFactoryManager::UpdateHoveredCellFromMouseRaycast()
+{
+	FGridCoord NewHoveredCellCoord;
+	if (!TryGetMouseRaycastGridCoord(NewHoveredCellCoord) || !GetCell(NewHoveredCellCoord))
+	{
+		if (bHasPreviousHoveredCellCoord)
+		{
+			ClearDebugHoveredCell();
+			bHasPreviousHoveredCellCoord = false;
+		}
+
+		return;
+	}
+
+	if (bHasPreviousHoveredCellCoord && PreviousHoveredCellCoord == NewHoveredCellCoord)
+	{
+		return;
+	}
+
+	PreviousHoveredCellCoord = NewHoveredCellCoord;
+	bHasPreviousHoveredCellCoord = true;
+	SetDebugHoveredCell(NewHoveredCellCoord);
+}
+
+bool AFactoryManager::TryGetMouseRaycastGridCoord(FGridCoord& OutCoord) const
+{
+	const UWorld* World = GetWorld();
+	if (!World)
+	{
+		return false;
+	}
+
+	const APlayerController* PlayerController = World->GetFirstPlayerController();
+	if (!PlayerController)
+	{
+		return false;
+	}
+
+	FVector RayOrigin;
+	FVector RayDirection;
+	if (!PlayerController->DeprojectMousePositionToWorld(RayOrigin, RayDirection) || FMath::IsNearlyZero(RayDirection.Z))
+	{
+		return false;
+	}
+
+	const float PlaneZ = GetActorLocation().Z;
+	const float DistanceToPlane = (PlaneZ - RayOrigin.Z) / RayDirection.Z;
+	if (DistanceToPlane < 0.0f)
+	{
+		return false;
+	}
+
+	const FVector HitLocation = RayOrigin + RayDirection * DistanceToPlane;
+	OutCoord = WorldLocationToGridCoord(HitLocation);
+	return true;
+}
+
+void AFactoryManager::DrawDebugGrid() const
+{
+	if (!GetWorld())
+	{
+		return;
+	}
+
+	for (const TPair<FGridCoord, FFactoryChunk>& ChunkPair : Chunks)
+	{
+		const FGridCoord& ChunkCoord = ChunkPair.Key;
+
+		if (bDrawDebugCells)
+		{
+			DrawDebugCellGridForChunk(ChunkCoord);
+		}
+
+		if (bDrawDebugChunks)
+		{
+			DrawDebugChunkBounds(ChunkCoord);
+		}
+	}
+
+	if (bDrawDebugHoveredCell && bHasDebugHoveredCell && GetCell(DebugHoveredCellCoord))
+	{
+		DrawDebugCellBounds(DebugHoveredCellCoord, DebugHoveredCellColor, DebugHoveredCellLineThickness);
+	}
+}
+
+void AFactoryManager::DrawDebugCellGridForChunk(const FGridCoord& ChunkCoord) const
+{
+	const int32 ChunkMinX = ChunkCoord.X * FFactoryChunk::ChunkSize;
+	const int32 ChunkMinY = ChunkCoord.Y * FFactoryChunk::ChunkSize;
+	const int32 ChunkMaxX = ChunkMinX + FFactoryChunk::ChunkSize;
+	const int32 ChunkMaxY = ChunkMinY + FFactoryChunk::ChunkSize;
+
+	for (int32 LineOffset = 0; LineOffset <= FFactoryChunk::ChunkSize; ++LineOffset)
+	{
+		const float GridX = ChunkMinX + LineOffset - 0.5f;
+		const float GridY = ChunkMinY + LineOffset - 0.5f;
+
+		DrawDebugLine(
+			GetWorld(),
+			GridBoundaryToWorld(GridX, ChunkMinY - 0.5f),
+			GridBoundaryToWorld(GridX, ChunkMaxY - 0.5f),
+			DebugCellColor,
+			false,
+			0.0f,
+			0,
+			DebugCellLineThickness
+		);
+
+		DrawDebugLine(
+			GetWorld(),
+			GridBoundaryToWorld(ChunkMinX - 0.5f, GridY),
+			GridBoundaryToWorld(ChunkMaxX - 0.5f, GridY),
+			DebugCellColor,
+			false,
+			0.0f,
+			0,
+			DebugCellLineThickness
+		);
+	}
+}
+
+void AFactoryManager::DrawDebugChunkBounds(const FGridCoord& ChunkCoord) const
+{
+	const int32 ChunkMinX = ChunkCoord.X * FFactoryChunk::ChunkSize;
+	const int32 ChunkMinY = ChunkCoord.Y * FFactoryChunk::ChunkSize;
+	const int32 ChunkMaxX = ChunkMinX + FFactoryChunk::ChunkSize - 1;
+	const int32 ChunkMaxY = ChunkMinY + FFactoryChunk::ChunkSize - 1;
+
+	DrawDebugCellBounds(
+		FGridCoord(ChunkMinX, ChunkMinY),
+		FGridCoord(ChunkMaxX, ChunkMaxY),
+		DebugChunkColor,
+		DebugChunkLineThickness
+	);
+}
+
+void AFactoryManager::DrawDebugCellBounds(const FGridCoord& Coord, const FColor& Color, float Thickness) const
+{
+	DrawDebugCellBounds(Coord, Coord, Color, Thickness);
+}
+
+void AFactoryManager::DrawDebugCellBounds(
+	const FGridCoord& MinCoord,
+	const FGridCoord& MaxCoord,
+	const FColor& Color,
+	float Thickness
+) const
+{
+	const FVector BottomLeft = GridBoundaryToWorld(MinCoord.X - 0.5f, MinCoord.Y - 0.5f);
+	const FVector BottomRight = GridBoundaryToWorld(MaxCoord.X + 0.5f, MinCoord.Y - 0.5f);
+	const FVector TopRight = GridBoundaryToWorld(MaxCoord.X + 0.5f, MaxCoord.Y + 0.5f);
+	const FVector TopLeft = GridBoundaryToWorld(MinCoord.X - 0.5f, MaxCoord.Y + 0.5f);
+
+	DrawDebugLine(GetWorld(), BottomLeft, BottomRight, Color, false, 0.0f, 0, Thickness);
+	DrawDebugLine(GetWorld(), BottomRight, TopRight, Color, false, 0.0f, 0, Thickness);
+	DrawDebugLine(GetWorld(), TopRight, TopLeft, Color, false, 0.0f, 0, Thickness);
+	DrawDebugLine(GetWorld(), TopLeft, BottomLeft, Color, false, 0.0f, 0, Thickness);
+}
+
+FGridCoord AFactoryManager::WorldLocationToGridCoord(const FVector& WorldLocation) const
+{
+	const FVector RelativeLocation = WorldLocation - GetActorLocation();
+	return FGridCoord(
+		FMath::FloorToInt(RelativeLocation.X / CellSize + 0.5f),
+		FMath::FloorToInt(RelativeLocation.Y / CellSize + 0.5f)
+	);
+}
+
+FVector AFactoryManager::GridCellCenterToWorld(const FGridCoord& Coord) const
+{
+	return GetActorLocation() + FVector(Coord.X * CellSize, Coord.Y * CellSize, 0.0f);
+}
+
+FVector AFactoryManager::GridBoundaryToWorld(float BoundaryX, float BoundaryY) const
+{
+	return GetActorLocation() + FVector(BoundaryX * CellSize, BoundaryY * CellSize, DebugGridZOffset);
 }
 
 FGridCoord AFactoryManager::WorldCoordToChunkCoord(const FGridCoord& WorldCoord) const
--- Source/FactoryExperiment/Public/Factory/FactoryManager.h
+++ Source/FactoryExperiment/Public/Factory/FactoryManager.h
@@ -12,6 +12,7 @@
 #include "FactoryManager.generated.h"
 
 class AFactoryBuilding;
+class USceneComponent;
 class UFactoryBuildingDataAsset;
 class UFactoryRecipeDataAsset;
 
@@ -25,13 +26,32 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Factory")
 	TObjectPtr<UFactoryRecipeDataAsset> RecipeDatabase;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Factory")
+	TObjectPtr<USceneComponent> TransformComponent;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Factory")
 	float SimulationStepInterval = 0.2f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Factory|Debug")
+	bool bDrawDebugCells = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Factory|Debug")
+	bool bDrawDebugChunks = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Factory|Debug")
+	bool bDrawDebugHoveredCell = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Factory|Debug")
+	bool bUpdateHoveredCellFromMouseRaycast = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Factory|Debug")
+	FGridCoord DebugHoveredCellCoord;
 
 private:
 	UPROPERTY()
@@ -75,14 +95,39 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Factory")
 	TArray<FGridCoord> GetNeighbors(const FGridCoord& Coord) const;
 
+	UFUNCTION(BlueprintCallable, Category = "Factory|Debug")
+	void SetDebugHoveredCell(const FGridCoord& Coord);
+
+	UFUNCTION(BlueprintCallable, Category = "Factory|Debug")
+	void ClearDebugHoveredCell();
+
 private:
+	static constexpr float CellSize = 100.0f;
+
 	void SimulationStep();
 
 	void UpdateMachines(float DeltaTime);
 	void UpdateConveyors(float DeltaTime);
 
+	void CreateInitialChunks();
+	void UpdateHoveredCellFromMouseRaycast();
+	bool TryGetMouseRaycastGridCoord(FGridCoord& OutCoord) const;
+	void DrawDebugGrid() const;
+	void DrawDebugCellGridForChunk(const FGridCoord& ChunkCoord) const;
+	void DrawDebugChunkBounds(const FGridCoord& ChunkCoord) const;
+	void DrawDebugCellBounds(const FGridCoord& Coord, const FColor& Color, float Thickness) const;
+	void DrawDebugCellBounds(const FGridCoord& MinCoord, const FGridCoord& MaxCoord, const FColor& Color, float Thickness) const;
+
+	FGridCoord WorldLocationToGridCoord(const FVector& WorldLocation) const;
+	FVector GridCellCenterToWorld(const FGridCoord& Coord) const;
+	FVector GridBoundaryToWorld(float BoundaryX, float BoundaryY) const;
+
 	FGridCoord WorldCoordToChunkCoord(const FGridCoord& WorldCoord) const;
 	FGridCoord WorldCoordToLocalCoord(const FGridCoord& WorldCoord) const;
 
 	int32 RegisterBuildingActor(AFactoryBuilding* Building);
+
+	bool bHasDebugHoveredCell = false;
+	bool bHasPreviousHoveredCellCoord = false;
+	FGridCoord PreviousHoveredCellCoord;
 };
